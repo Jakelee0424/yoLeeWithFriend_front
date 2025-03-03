00b321395e859452d02e3a0443fd1ecbcaaa4d38
gamewithfriend <jse3685545@naver.com>
2025-03-03 14:44:41 +0900

    git commit fullLayout width 100% mod

--- src/pages/admin/template/FullLayout.js
+++ src/pages/admin/template/FullLayout.js
@@ -14,7 +14,7 @@ const FullLayout = () => {
           <Sidebar />
         </aside>
         {/********Content Area**********/}
-        <div className="contentArea"> 
+        <div className="contentArea" style={{width:"100%"}}> 
           {/********Middle Content**********/}
           <Container className="p-4" fluid>
             <Outlet />
