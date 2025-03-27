69aaf300b6094e5ed651575c24636ecabe03b03e
gamewithfriend <jse3685545@naver.com>
2025-03-27 21:17:53 +0900

    관리자 목록 및 관리자 상세정보 커밋

---
+++ src/service/admin/admin/adminService.js
@@ -0,0 +1,31 @@
+import Fetcher from 'utils/Fetcher';
+
+export const fetcherAdminList = async (data) => {
+
+      const fetcher = new Fetcher().setUrl("/admin/all")
+                                         .setMethod("POST")
+                                         .setData(JSON.stringify(data));
+      try {
+        const result = await fetcher.jsonFetch();
+        return result;
+        //console.log("result : ", result.data);
+      } catch (error) {
+        console.error('login error:', error);
+      }
+
+};
+
+export const fetcherAdmin = async (data) => {
+
+  const fetcher = new Fetcher().setUrl("/admin/"+data.id)
+                                     .setMethod("GET");
+  try {
+    const result = await fetcher.jsonFetch();
+    return result;
+    //console.log("result : ", result.data);
+  } catch (error) {
+    console.error('login error:', error);
+  }
+
+};
+
