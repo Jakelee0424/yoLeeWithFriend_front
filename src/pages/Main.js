df4765c638ef9a3e90f2f8c9c4774338a247e93c
gamewithfriend <jse3685545@naver.com>
2025-07-15 22:50:33 +0900

    [유저] 메인 및 뉴스레터 칸 세팅

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,9 +13,53 @@ function Main() {
   };
 
   return (
-    <div className="App">
-      <h1>유저 화면 분할 표시 안녕하세요!</h1>
+    <div className="App" style={{width:"100%",height:"100%"}}>
       <button onClick={goToTest}>차단 테스트 페이지로 이동</button>
+      <div style={{
+        display:"flex"
+      }}>
+        <div style={{
+          backgroundColor:"yellow"
+          ,width:"67%"
+          ,height:"18rem"
+          ,marginRight:"3%"
+        }}>
+          ss
+        </div>
+        <div style={{
+          backgroundColor:"black"
+          ,width:"30%"
+          ,height:"18rem"
+        }}>
+          ss
+        </div>
+      </div>
+      <div style={{
+        width:"100%",
+        height:"13rem",
+        marginTop:"3%",
+        backgroundColor:"black"
+      }}>
+        
+      </div>
+      <div style={{
+        width:"100%",
+        height:"40rem",
+        marginTop:"3%",
+        backgroundColor:"black"
+      }}>
+        
+      </div>
+      {/* 뉴스레터 div */}
+      <div style={{
+        width:"100%",
+        height:"20rem",
+        marginTop:"3%",
+        backgroundColor:"black"
+      }}>
+        
+      </div>
+      {/* 뉴스레터 div */}
     </div>
   );
 }
