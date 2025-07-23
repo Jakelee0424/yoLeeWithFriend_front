0a8064bc1cc1596a5774e2ad141708a48b4b0fe2
LAPTOP-QGU8C8GR\user <jklee@precursor.co.kr>
2025-07-23 15:33:23 +0900

    보충제 비교 업데이트

--- src/pages/user/board/versus/BoardVersus.js
+++ src/pages/user/board/versus/BoardVersus.js
@@ -1,5 +1,6 @@
 import { useEffect,React,useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, ButtonGroup } from 'reactstrap';
+import VersusModal from "./VersusModal.js"
 import versusStyle from "style/versus.module.css"
 import fontstyles from 'style/font.module.css';
 
@@ -8,26 +9,31 @@ function BoardVersus() {
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
 
-  const [rSelected, setRSelected] = useState(null);
-
+  const [selectedBoard, setSelectedBoard] = useState([]);
 
   return (
     <div className="App">
 
       <div className={versusStyle.buttonContainer}>
         <div className={versusStyle.buttonGroup}>
-          <button className={versusStyle.pickButton} onClick={toggle}>
-            <p>+</p>
-            <p className={fontstyles.text}>보충제를 선택하세요</p>
-          </button>
-          <button className={versusStyle.pickButton} onClick={toggle}>
-            <p>+</p>
-            <p className={fontstyles.text}>보충제를 선택하세요</p>
-          </button>
-          <button className={versusStyle.pickButton} onClick={toggle}>
-            <p>+</p>
-            <p className={fontstyles.text}>보충제를 선택하세요</p>
-          </button>
+          {[0, 1, 2].map((index) => (
+            <button
+              key={index}
+              className={versusStyle.pickButton}
+              onClick={toggle}
+            >
+              {selectedBoard[index] ? (
+                <p className={fontstyles.text}>
+                  {selectedBoard[index].boardName}
+                </p>
+              ) : (
+                <>
+                  <p>+</p>
+                  <p className={fontstyles.text}>보충제를 선택하세요</p>
+                </>
+              )}
+            </button>
+          ))}
         </div>      
       </div>
 
@@ -47,50 +53,13 @@ function BoardVersus() {
         <p className={fontstyles.text}> 평가 정보 </p>
       </div>
 
-
-
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle} className={fontstyles.text}>보충제 검색</ModalHeader>
-        <ModalBody>
-          <div>
-            <div className={versusStyle.pickKind}>
-              <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>보충제</p>
-              <div className={versusStyle.buttonContainerInModal}>
-                <Button
-                  color="secondary"
-                  outline
-                  onClick={() => setRSelected(1)}
-                  active={rSelected === 1}
-                >
-                  BCAA
-                </Button>
-                <Button
-                  color="secondary"
-                  outline
-                  onClick={() => setRSelected(2)}
-                  active={rSelected === 2}
-                >
-                  프로틴
-                </Button>
-                <Button
-                  color="secondary"
-                  outline
-                  onClick={() => setRSelected(3)}
-                  active={rSelected === 3}
-                >
-                  부스터
-                </Button>
-              </div>
-            </div>
-
-
-
-          </div>
-
-
-        </ModalBody>
-      </Modal>
-
+      <VersusModal 
+        modal={modal} 
+        setModal={setModal}
+        toggle={toggle}
+        selectedBoard = {selectedBoard}
+        setSelectedBoard = {setSelectedBoard}
+      />
 
     </div>
   );
