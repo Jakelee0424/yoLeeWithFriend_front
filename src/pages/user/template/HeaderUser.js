e0a0ffd7d75167795650ed4de7dab2d10af87d46
gamewithfriend <jse3685545@naver.com>
2025-07-29 21:52:17 +0900

    [유저]로그인 테스트 용

--- src/pages/user/template/HeaderUser.js
+++ src/pages/user/template/HeaderUser.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import {
   Navbar,
@@ -17,6 +17,7 @@ const Header = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const [dropdownOpen, setDropdownOpen] = React.useState(false);
   const { menuTree } = useMenu();
+  const [getUserName, setUserName] = useState("");
   const toggle = () => setDropdownOpen((prevState) => !prevState);
   const Handletoggle = () => setIsOpen(!isOpen);
   const showMobilemenu = () => {
@@ -39,7 +40,7 @@ const Header = () => {
       try {
         const result = await fetcher.jsonFetch();
         dispatch({type:"PLUS_ONE",payload: result.data})
-        console.log("sss")
+        setUserName(result.data.nickName)
       } catch (error) {
         console.error('login error:', error);
       }
@@ -186,7 +187,7 @@ const Header = () => {
           width: "15%",
           height: "5rem", // 80px
         }}
-      ></div>
+      >{token ? getUserName : ""}</div>
     </Navbar>
   );
 };
