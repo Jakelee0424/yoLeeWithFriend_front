805644b88b9d575b526461a90c33830d6aee2714
LAPTOP-QGU8C8GR\user <jklee@precursor.co.kr>
2025-08-20 18:52:18 +0900

    모달 완성

--- src/pages/user/board/versus/VersusModal.js
+++ src/pages/user/board/versus/VersusModal.js
@@ -1,76 +1,75 @@
 import { useEffect, useState } from "react";
-import { Button, ButtonGroup, Modal, ModalHeader, ModalBody, Input, Card, CardBody, CardSubtitle, Row, Col} from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, Input, Card, CardBody, CardSubtitle, Row, Col } from "reactstrap";
 import Blog from "../../../../otherLib/bootStrap/components/dashboard/Blog";
 import versusStyle from "style/versus.module.css";
 import fontstyles from "style/font.module.css";
 import * as boardMngrService from "service/admin/boardMngr/boardMngrService";
 import * as boardService from "service/user/boardMngr/boardService";
-import { getCodeNameByIdApi, getCodeListByParentIdApi } from 'utils/CodeUtil';
 
-function VersusModal({modal, toggle, selectedBoard, setSelectedBoard, selectedIndex, setNuinfoList, getNuinfoList}) {
-
+function VersusModal({ modal, toggle, selectedBoard, setSelectedBoard, selectedIndex, setNuinfoList, getNuinfoList }) {
   const [brandList, setBrandList] = useState([]);
   const [queryParam, setQueryParam] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [getBoardMngrList, setBoardMngrList] = useState([]);
-  const [brandId, setBrandId] = useState("");
-  const [selected, setSelected] = useState(""); 
+  const [getBoardList, setBoardList] = useState([]);
+  const [brandId, setBrandId] = useState("boardBand00");
+  const [selected, setSelected] = useState("all");
 
-  const getBoardMngrListByFetcher = async () => {
-    setQueryParam("");
-    setBrandId("boardBand00");
+  // 보충제 목록 가져오기
+  const getBoardListByFetcher = async () => {
+
+    let type;
+    let brandId;
+    let queryParam;
+
+    if (selectedBoard && selectedBoard.some(item => item && item.boardCategoryCodeId)) {
+      type = selected;
+    } else {
+      type = "all";
+      brandId = "boardBand00";
+      queryParam = "";
+    }
 
     const inputData = {
-      queryParam: queryParam,
-      currentPage: currentPage,
-      type : "all",
+      queryParam : queryParam,
+      currentPage,
+      type: type,
       itemsPerPage: 20,
       pageBlockSize: 10,
-      brandId: brandId
+      brandId : brandId
     };
 
-    boardMngrService.fetcherBoardMngrList(inputData).then((outPutData) => {
-      setBoardMngrList(outPutData.data.content);
+    boardService.fetcherBoardFind(inputData).then((outPutData) => {
+      setBoardList(outPutData.data);
     });
   };
 
+  // 브랜드 목록 가져오기
   const getBrandListByFetcher = async () => {
     boardService.fetcherBrandList().then((outPutData) => {
       setBrandList([{ id: "boardBand00", name: "전체" }, ...(outPutData.data || [])]);
     });
   };
-  
+
+  // 카테고리 선택
   const onClickCategory = (category) => {
     setSelected(category);
-    
-    // const inputData = {
-    //   queryParam: queryParam,
-    //   currentPage: currentPage,
-    //   type : category,
-    //   itemsPerPage: 8,
-    //   pageBlockSize: 10
-    // };
-
-    // boardMngrService.fetcherBoardMngrList(inputData).then((outPutData) => {
-    //   setBoardMngrList(outPutData.data.content);
-    // });
-
   };
 
+  // 검색
   const searchBoardList = () => {
     const inputData = {
-      queryParam: queryParam,
-      currentPage: currentPage,
-      type : selected,
+      queryParam,
+      currentPage,
+      type: selected,
       itemsPerPage: 8,
       pageBlockSize: 10,
-      brandId: brandId
+      brandId
     };
 
     boardService.fetcherBoardFind(inputData).then((outPutData) => {
-      setBoardMngrList(outPutData.data);
+      setBoardList(outPutData.data);
     });
-  }
+  };
 
   // 모달 안에서 보충제 클릭 시 해당 칸에 삽입
   const onBoardClickInVersus = (tdata) => {
@@ -82,20 +81,39 @@ function VersusModal({modal, toggle, selectedBoard, setSelectedBoard, selectedIn
       });
     }
 
-    boardMngrService.fetcherBoard(tdata).then(async (outPutData) => {
-      console.log(tdata);
+    setSelected(tdata.boardCategoryCodeId);
 
+    boardMngrService.fetcherBoard(tdata).then(async (outPutData) => {
+      console.log("선택된 보충제:", tdata);
       // setNuinfoList(await getCodeListByParentIdApi(outPutData.data.boardMngrResDto.nuinfoId))
-    })
+    });
 
     toggle();
   };
 
   useEffect(() => {
-    getBoardMngrListByFetcher();
+    getBoardListByFetcher();
     getBrandListByFetcher();
-    setSelected("all");
   }, []);
+
+  // 모달 오픈 시 인풋값 초기화, selectedBoard가 있으면 type만 고정
+  useEffect(() => {
+    if (modal) {
+      const hasSelectedBoard =
+        selectedBoard && selectedBoard.some((item) => item && item.boardCategoryCodeId);
+
+      setBrandId("boardBand00");
+      setQueryParam("");
+
+      if (!hasSelectedBoard) {
+        setSelected("all");
+      }
+    }
+  }, [modal, selectedBoard]);
+
+  useEffect(() => {
+    searchBoardList();
+  }, [selected, brandId])
 
   return (
     <Modal isOpen={modal} toggle={toggle} size="lg">
@@ -105,57 +123,59 @@ function VersusModal({modal, toggle, selectedBoard, setSelectedBoard, selectedIn
       <ModalBody>
         {/* 카테고리 선택 */}
         <div className={versusStyle.pickKind}>
-          <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>
-            보충제
-          </p>
+          <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>보충제</p>
           <div className={versusStyle.buttonContainerInModal}>
-               <Button
-                color="dark"
-                outline={selected !== "all"}
-                onClick={() => onClickCategory("all")}
-                style={{ width: "100px", margin: "5px" }}
-              >
-                전체
-              </Button>
+            <Button
+              color="dark"
+              outline={selected !== "all"}
+              onClick={() => onClickCategory("all")}
+              style={{ width: "100px", margin: "5px" }}
+              disabled={selectedBoard.some((item) => item !== null)} 
+            >
+              전체
+            </Button>
 
-              <Button
-                color="dark"
-                outline={selected !== "boardCategory02"}
-                onClick={() => onClickCategory("boardCategory02")}
-                style={{ width: "100px", margin: "5px" }}
-              >
-                BCAA
-              </Button>
+            <Button
+              color="dark"
+              outline={selected !== "boardCategory02"}
+              onClick={() => onClickCategory("boardCategory02")}
+              style={{ width: "100px", margin: "5px" }}
+              disabled={selectedBoard.some((item) => item !== null)} 
+            >
+              BCAA
+            </Button>
 
-              <Button
-                color="dark"
-                outline={selected !== "boardCategory01"}
-                onClick={() => onClickCategory("boardCategory01")}
-                style={{ width: "100px", margin: "5px" }}
-              >
-                프로틴
-              </Button>
+            <Button
+              color="dark"
+              outline={selected !== "boardCategory01"}
+              onClick={() => onClickCategory("boardCategory01")}
+              style={{ width: "100px", margin: "5px" }}
+              disabled={selectedBoard.some((item) => item !== null)} 
+            >
+              프로틴
+            </Button>
 
-              <Button
-                color="dark"
-                outline={selected !== "boardCategory03"}
-                onClick={() => onClickCategory("boardCategory03")}
-                style={{ width: "100px", margin: "5px" }}
-              >
-                부스터
-              </Button>
+            <Button
+              color="dark"
+              outline={selected !== "boardCategory03"}
+              onClick={() => onClickCategory("boardCategory03")}
+              style={{ width: "100px", margin: "5px" }}
+              disabled={selectedBoard.some((item) => item !== null)} 
+            >
+              부스터
+            </Button>
           </div>
         </div>
 
         {/* 브랜드 선택 */}
         <div className={versusStyle.pickKind}>
-          <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>
-            브랜드
-          </p>
+          <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>브랜드</p>
           <div className={versusStyle.buttonContainerInModal}>
             <Input type="select" value={brandId} onChange={(e) => setBrandId(e.target.value)}>
-              {brandList.map((brandList, index) => (
-                <option key={index} value={brandList.id}>{brandList.name}</option>
+              {brandList.map((brand, index) => (
+                <option key={index} value={brand.id}>
+                  {brand.name}
+                </option>
               ))}
             </Input>
           </div>
@@ -163,16 +183,14 @@ function VersusModal({modal, toggle, selectedBoard, setSelectedBoard, selectedIn
 
         {/* 검색 */}
         <div className={versusStyle.pickKind}>
-          <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>
-            검색
-          </p>
+          <p className={`${versusStyle.titleInModal} ${fontstyles.text}`}>검색</p>
           <div className={versusStyle.buttonContainerInModal}>
             <Input
               type="text"
               value={queryParam}
               onChange={(e) => setQueryParam(e.target.value)}
             />
-            <Button color="secondary" outline onClick={() => searchBoardList()}>
+            <Button color="secondary" outline onClick={searchBoardList}>
               검색
             </Button>
           </div>
@@ -183,17 +201,16 @@ function VersusModal({modal, toggle, selectedBoard, setSelectedBoard, selectedIn
         {/* 보충제 리스트 */}
         <Card style={{ width: "100%" }}>
           <CardBody>
-            <CardSubtitle className="mb-2 text-muted" tag="h6" style={{ display: "flex" }}>
-            </CardSubtitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6" style={{ display: "flex" }} />
             <div
               style={{
                 height: "calc(8 * 3rem)", // 높이는 8개 정도만 보이게
                 overflowX: "hidden",
-                minHeight: "0",
+                minHeight: "0"
               }}
             >
               <Row>
-                {getBoardMngrList.map((tdata, index) => (
+                {getBoardList.map((tdata, index) => (
                   <Col
                     sm="6"
                     lg="6"
@@ -202,11 +219,7 @@ function VersusModal({modal, toggle, selectedBoard, setSelectedBoard, selectedIn
                     style={{ cursor: "pointer" }}
                     onClick={() => onBoardClickInVersus(tdata)}
                   >
-                    <div style={{ flex: 1 }}></div>
-                    <Blog
-                      className={versusStyle.boardList}
-                      title={tdata.boardName}
-                    />
+                    <Blog className={versusStyle.boardList} title={tdata.boardName} />
                   </Col>
                 ))}
               </Row>
